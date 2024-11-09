ae7492b6208264aba9f72093ad7f4283f692ffb8
StanislavVelykoivan <velykoivanstas@gmail.com>
2024-11-09 01:53:20 +0100

    linear search

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int nums[] = {8,4,9,2,6};
+        int target = 2;
 
+        int result = linearSearch(nums, target);
+        if (result != -1)
+            System.out.println(result);
+        else
+            System.out.println("Not found");
     }
+
+    public static int linearSearch(int[] nums, int target) {
+        for (int num : nums) {
+            if (target == num)
+                return num;
+        }
+        return -1;
+    }
+
 }
