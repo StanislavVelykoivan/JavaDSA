ec4d972b07e9083bf452f437b8106dd539892d7e
StanislavVelykoivan <velykoivanstas@gmail.com>
2024-11-09 02:23:07 +0100

    added steps counter

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,24 @@ public class Main {
 
         int result = binarySearch(nums, target);
         if (result != -1)
-            System.out.println(result);
+            System.out.println("Result: " + result);
         else
             System.out.println("Not found");
     }
 
     public static int binarySearch(int[] nums, int target) {
 
+        int steps = 0;
         int left = 0;
         int right = nums.length - 1;
 
         while (left <= right) {
+            steps++;
+
             int mid = (left + right) / 2;
 
             if (nums[mid] == target) {
+                System.out.println("Steps: " + steps);
                 return mid;
             } else if (nums[mid] < target) {
                 left = mid + 1;
@@ -27,7 +31,7 @@ public class Main {
                 right = mid - 1;
             }
         }
-
+        System.out.println("Steps: " + steps);
         return -1;
     }
 
