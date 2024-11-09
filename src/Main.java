acd0f0b2ee97bab507d70b9f85aea98531afd2bb
StanislavVelykoivan <velykoivanstas@gmail.com>
2024-11-09 02:10:19 +0100

    binary search

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
-        int nums[] = {8,4,9,2,6};
-        int target = 2;
+        int[] nums = {2, 4, 6, 8, 9};
+        int target = 8;
 
-        int result = linearSearch(nums, target);
+        int result = binarySearch(nums, target);
         if (result != -1)
             System.out.println(result);
         else
             System.out.println("Not found");
     }
 
-    public static int linearSearch(int[] nums, int target) {
-        for (int num : nums) {
-            if (target == num)
-                return num;
+    public static int binarySearch(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
+
         return -1;
     }
 
