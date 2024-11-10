c302e25888a29cb011d42db234b35896b53495d4
StanislavVelykoivan <velykoivanstas@gmail.com>
2024-11-10 21:16:29 +0100

    added recurrent

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,49 @@ public class Main {
         int[] nums = {2, 4, 6, 8, 9};
         int target = 8;
 
-        int result = binarySearch(nums, target);
+        int result = binarySearch(nums, target, 0, nums.length - 1);
         if (result != -1)
             System.out.println("Result: " + result);
         else
             System.out.println("Not found");
     }
 
-    public static int binarySearch(int[] nums, int target) {
+    static int steps = 0;
 
-        int steps = 0;
-        int left = 0;
-        int right = nums.length - 1;
+    public static int binarySearch(int[] nums, int target, int left, int right) {
 
-        while (left <= right) {
+
+//        int left = 0;
+//        int right = nums.length - 1;
+
+        if (left <= right) {
             steps++;
-
             int mid = (left + right) / 2;
 
             if (nums[mid] == target) {
                 System.out.println("Steps: " + steps);
                 return mid;
             } else if (nums[mid] < target) {
-                left = mid + 1;
+                return binarySearch(nums, target, mid + 1, right);
             } else {
-                right = mid - 1;
+                return binarySearch(nums, target, left, mid - 1);
             }
         }
-        System.out.println("Steps: " + steps);
+//        while (left <= right) {
+//            steps++;
+//
+//            int mid = (left + right) / 2;
+//
+//            if (nums[mid] == target) {
+//                System.out.println("Steps: " + steps);
+//                return mid;
+//            } else if (nums[mid] < target) {
+//                left = mid + 1;
+//            } else {
+//                right = mid - 1;
+//            }
+//        }
+//        System.out.println("Steps: " + steps);
         return -1;
     }
 
