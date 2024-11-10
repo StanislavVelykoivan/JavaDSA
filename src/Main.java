e85a73cff27d32621467cb01866a997e9b6f52dc
StanislavVelykoivan <velykoivanstas@gmail.com>
2024-11-10 21:43:43 +0100

    bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] nums = {2, 4, 6, 8, 9};
-        int target = 8;
+        int[] nums = {6, 5, 2, 8, 9, 4};
 
-        int result = binarySearch(nums, target, 0, nums.length - 1);
-        if (result != -1)
-            System.out.println("Result: " + result);
-        else
-            System.out.println("Not found");
-    }
-
-    static int steps = 0;
-
-    public static int binarySearch(int[] nums, int target, int left, int right) {
-
-
-//        int left = 0;
-//        int right = nums.length - 1;
-
-        if (left <= right) {
-            steps++;
-            int mid = (left + right) / 2;
-
-            if (nums[mid] == target) {
-                System.out.println("Steps: " + steps);
-                return mid;
-            } else if (nums[mid] < target) {
-                return binarySearch(nums, target, mid + 1, right);
-            } else {
-                return binarySearch(nums, target, left, mid - 1);
-            }
+        System.out.println("Befor");
+        for (int num : nums) {
+            System.out.print(num + " ");
         }
-//        while (left <= right) {
-//            steps++;
-//
-//            int mid = (left + right) / 2;
-//
-//            if (nums[mid] == target) {
-//                System.out.println("Steps: " + steps);
-//                return mid;
-//            } else if (nums[mid] < target) {
-//                left = mid + 1;
-//            } else {
-//                right = mid - 1;
-//            }
-//        }
-//        System.out.println("Steps: " + steps);
-        return -1;
-    }
+        System.out.println();
 
+        int temp = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+
+        }
+
+        System.out.println("After");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
 }
