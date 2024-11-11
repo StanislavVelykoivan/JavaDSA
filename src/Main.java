b23dc5924d42833cc166c999980e3bd9c827c8e5
StanislavVelykoivan <velykoivanstas@gmail.com>
2024-11-11 13:07:13 +0100

    quick sort

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,40 @@ public class Main {
         }
         System.out.println();
 
-        for (int i = 0; i < nums.length; i++) {
-            int key = nums[i];
-            int j = i - 1;
-
-            while (j >= 0 && nums[j] > key) {
-                nums[j + 1] = nums[j];
-                j--;
-            }
-            nums[j + 1] = key;
-        }
+        quickSort(nums, 0, nums.length - 1);
 
         System.out.println("After");
         for (int num : nums) {
             System.out.print(num + " ");
         }
     }
+
+    public static int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        int temp = nums[i + 1];
+        nums[i + 1] = nums[high];
+        nums[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+
+            quickSort(nums, low, pi - 1);
+            quickSort(nums, pi + 1, high);
+        }
+    }
 }
+
